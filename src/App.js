eef325fc75897c0f8d9cc1f19306c7559680daab
mandah-israel <mandahisrael@gmail.com>
2022-06-01 18:49:35 +0100

    starter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './app.css';
+import { useQuery } from "react-query";
 
 function App() {
+
+  const fetchRepositories = async () => {
+    const response = await fetch(`https://api.github.com/search/repositories?q=topic:react`)
+    return response.json()
+  }
+
+  const {data, isSuccess} = useQuery("repos",  fetchRepositories)
+
   return (
     <div className="app">
-      
+      {isSuccess && data.items.map((comment) => (
+        <div className='result' key={comment.id}>
+          <span>{comment.name}</span>
+          <p>{comment.description}</p>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient({})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
